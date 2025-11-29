ff251807249f597362b38899b475abc170575ca2
Kavita Bhoite <kavita.bhoite11@gmail.com>
2025-11-29 20:24:31 +0530

    feature/clean-code

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
-let bubble = "";
+//move global variables on the top
+let timer = 60;
 
-// let nmBbl = Math.floor(Math.random()*100);
+//create bubbles
+function createBubble(){
+    let bubble = "";
 
-for(let i=1; i<154; i++){
-    let nm = Math.floor(Math.random()*10);
-    //add data the times loop runs 
-    bubble += `<div class="bubble">${nm}</div>`;
+    // let nmBbl = Math.floor(Math.random()*100);
+    for(let i=1; i<154; i++){
+        let nm = Math.floor(Math.random()*10);
+        //add data the times loop runs 
+        bubble += `<div class="bubble">${nm}</div>`;
+    }
+    let pbtm = document.querySelector('.pbtm');
+
+    //used innerHtml here bcuz appendChild doesn't accept strings it accepts html elements
+    pbtm.innerHTML = bubble;
 }
 
-let pbtm = document.querySelector('.pbtm');
+//run timer for 1 min
+function runTimer(){
+    setInterval(function(){
+        timer--;
+        let time = document.querySelector('#time');
+        time.textContent = timer;
+    }, 1000)
+}
 
-//used innerHtml here bcuz appendChild doesn't accept strings it accepts html elements
-pbtm.innerHTML = bubble;
+function setHit(){
+    let hit = Math.floor(Math.random()*10);
+    let hits = document.querySelector('#hitnm');
+    hits.textContent = hit;
+}
+
+createBubble();
+runTimer();
+setHit();
